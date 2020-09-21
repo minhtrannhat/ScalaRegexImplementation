dcd120bf335560754fd63314cc9d90d11adc86cb
minhradz <minhradz@gmail.com>
2020-09-20 23:10:42 -0400

    Feat: Parsers functions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -32,7 +32,48 @@ case class Plus(expr: RegexExpr) extends RegexExpr
 // we need 4 different types of expressions.
 // We named them lit, lowExpr(+, *), midExpr(concatenation) and highExpr(|)
 object RegexParser extends RegexParsers {
+  // The ".r" indicates that the part inside 6 quotation marks is a regex expression
+  // Find something that matched a word OR a period
   def charLit: Parser[RegexExpr] = ("""\w""".r| ".") ^^ {
-    char => Literal(char.head)
+    // The first character of the first word of a string is parsed
+    string => Literal(string.head)
+  }
+
+  def parenExpr: Parser[RegexExpr] = "(" ~> highExpr <~ ")"
+
+  def lit: Parser[RegexExpr] = charLit | parenExpr
+
+  def repeat: Parser[RegexExpr] = lit <~ "*" ^^ {
+    case l => Repeat(l) 
+  }
+
+  def plus: Parser[RegexExpr] = lit <~ "+" ^^ {
+    case p => Plus(p) 
+  }
+
+  def lowExpr: Parser[RegexExpr] = repeat | plus | lit
+
+  def concat: Parser[RegexExpr] = rep(lowExpr) ^^ {
+    case list => listToConcat(list)
+  }
+
+  def midExpr: Parser[RegexExpr] = concat | lowExpr
+
+  def or: Parser[RegexExpr] = midExpr ~ "|" ~ midExpr ^^ {
+    case l ~ "|" ~ r => Or(l, r) 
+  }
+
+  def highExpr: Parser[RegexExpr] = or | midExpr
+
+  def listToConcat(list: List[RegexExpr]): RegexExpr = list match {
+    case head :: Nil => head
+    case head:: rest => Concat(head, listToConcat(rest))
+  }
+
+  def apply(input: String): Option[RegexExpr] = {
+    parseAll(highExpr, input) match {
+      case Success(result, _) => Some(result)
+      case failure : NoSuccess => None
+    }
   }
 }
