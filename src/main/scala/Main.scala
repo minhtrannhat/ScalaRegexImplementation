bf7e83e7f8e2e2fc180b0005c4625fc9d8addb76
minhradz <minhradz@gmail.com>
2020-11-03 09:51:26 -0500

    Feat: basic skeleton of the NFA

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -79,3 +79,49 @@ object RegexParser extends RegexParsers {
     }
   }
 }
+
+abstract class State
+
+class Consume(val c: Char, val out: State) extends State
+class Split(val out1: State, val out2:State) extends State
+
+// case class were used because they make all Match objects equal
+case class Match() extends State
+
+class Placeholder(var pointingtTo: State) extends State
+
+object NFA {
+  def regexToNFA(regex: RegexExpr): State =
+    regexToNFA(regex, Match())
+
+  private def regexToNFA(regex: RegexExpr,
+    andThen: State) : State = {
+      regex match {
+        case Literal(c) => new Consume(c, andThen)
+
+        case Concat(first, second) => {
+          // Convert the first to an NFA. The "out" of that is the result of converting second to an NFA
+          regexToNFA(first, regexToNFA(second, andThen))
+        }
+
+        case Or(l, r) => new Split(
+          regexToNFA(l, andThen)
+          regexToNFA(r, andThen)
+        )
+
+        case Repeat(r) => 
+          val placeholder = new Placeholder(null)
+          val split = new Split(
+            // one path goes to the placeholder
+            // the other path goes back to the r
+            regexToNFA(r, placeholder),
+            andThen
+          )
+          placeholder.pointingtTo = split
+          placeholder
+        
+        case Plus(r) => 
+          regexToNFA(Concat(r, Repeat(r)), andThen)
+      }
+  }
+}
